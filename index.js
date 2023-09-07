5023969ec79a8869dad43c3ed017c00722705273
Charity <99490614+CWhytte@users.noreply.github.com>
2023-09-07 21:04:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function updateDateTime() {
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const now = new Date();
     const dayOfWeek = daysOfWeek[now.getUTCDay()];
-    const utcTime = now.getUTCHours() + ':' + now.getUTCMinutes() + ' UTC';
+    const utcTime = now.getTime() + ':' + now.getUTCMinutes() + ' UTC';
 
     document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = dayOfWeek;
     document.querySelector('[data-testid="currentUTCTime"]').textContent = utcTime;
